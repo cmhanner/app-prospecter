7ad453097113a5ba904807da8d69aabc6fa840f3
cmhanner <chanceforcode@gmail.com>
2025-10-27 16:52:57 -0400

    Got Home Page and css started

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,44 @@
+import "./../css/Home.css"
+
 const Home = () => {
     return (
-        <div>
-            <h1>I'm Home</h1>
-        </div>
+        
+            <main id = "main-area" className = "main-content">
+                        <div id = "why-header">
+                            <h2>Why App Prospecter?</h2>
+                            <h2 id = "slogan">You find your next App Deal</h2>
+
+                            
+                        </div>
+                        <div id = "answer-area">
+                            <p>
+                                Sales is hard, but selling software is even harder. 
+                                What if there were a way to find apps with low reviews? 
+                                of September 7, 2025, there are nearly 2 million apps on the Apple App Store, with the number continuing to grow.
+                                How can we sort or find apps with low reviews? App Prospecter.
+                            </p>
+                        </div>
+
+                        <div id = "dashbord">
+                            
+                            <div id="dashboard-apps">
+                                
+                            
+                            
+                            </div>
+
+                            <div id ="dashboard-list">
+                                
+                                
+                            </div>
+
+
+
+
+                        </div>
+                        
+            </main>
+       
 
     );
 
