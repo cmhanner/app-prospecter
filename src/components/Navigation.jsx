5fda7ef47cd0fb969454b2833ae8f3d86d46ecab
cmhanner <chanceforcode@gmail.com>
2025-10-31 23:36:13 -0400

    Per Part 9, added JS toggle menu

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,25 @@
 import {Link} from "react-router-dom";
+import {useState} from "react";
 import "./../css/Navigation.css"
 
 const Navigation = () => {
+    const [toggleMenuOpen, setToggleMenuOpen] = useState(false);
+    const [toggleMenuClose, setToggleMenuClose] = useState(true);
+
+
+    const toggleMenu = (e) => {
+        e.preventDefault();
+        setToggleMenuOpen(!toggleMenuOpen);
+        setToggleMenuClose(!toggleMenuClose);
+    }
+
     return (
         
-            <nav id = "page-changer">
-                <ul>
+            <nav id = "page-changer" className = {toggleMenuOpen ? "" : "collapsed"}>
+                <a onClick = {toggleMenu} id= "toggle-nav" href = "#">
+                    {toggleMenuClose?(<p>☰</p>) : (<p>&larr;</p>)}
+                </a>
+                <ul className = {toggleMenuOpen? "": "hide-small"}>
                     <li><Link to = "/">Home</Link></li>
                     <li><Link to = "/Database">Database</Link></li>
                     <li><Link to = "/AddEdit">AddEdit</Link></li>
