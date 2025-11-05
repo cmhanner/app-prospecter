36709fc4969a0bbce966091a656c054ed829d511
cmhanner <chanceforcode@gmail.com>
2025-11-05 18:41:47 -0500

    Trying to get Images for Apps

--- src/api/appsApi.js
+++ src/api/appsApi.js
@@ -2,12 +2,14 @@
 
 import axios from "axios"; //  installed
 const BASE_URL = "https://cmhanner.github.io/csce242/projects/part6/app.json"; // easier to change later for Thursday
-
+// const IMG_URL = "https://cmhanner.github.io/tree/main/csce242/projects/part6/images_json";
 
 export async function getApps() {
     const response =  await axios.get(BASE_URL);
-    return response.data;
+    const image = await axios.get("https://cmhanner.github.io/tree/main/csce242/projects/part6/" + response.image); 
+    return response.data + image;
 }
+
 
 
 
