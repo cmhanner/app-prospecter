79aa2ccd5be047b5e3ae7ecbb2898cfee1618943
cmhanner <chanceforcode@gmail.com>
2025-11-08 21:58:57 -0500

    Version where API loads to Database, but no Images :(

--- src/api/appsApi.js
+++ src/api/appsApi.js
@@ -1,13 +1,17 @@
-//  appsApi.jsx
+//  appsApi.js
 
+import appImagePlaceholder from "./../images/app-placement-image.jpg";
 import axios from "axios"; //  installed
 const BASE_URL = "https://server-app-prospecter.onrender.com/api/apps"; // easier to change later for Thursday
-// const IMG_URL = "https://cmhanner.github.io/tree/main/csce242/projects/part6/images_json";
+const IMG_URL = "https://server-app-prospecter.onrender.com/images/";
 
 export async function getApps() {
     const response =  await axios.get(BASE_URL);
-    // const image = await axios.get("https://cmhanner.github.io/tree/main/csce242/projects/part6/" + response.image); 
-    return response.data;
+   const apps = response.data.map (app => ({
+    ...app,
+    imageUrl: app.image ? `${IMG_URL}${app.image}` : appImagePlaceholder
+   }));
+    return apps;
 }
 
 
