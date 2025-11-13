ee4d6913c267398b8ad04ddf69da128baa979060
cmhanner <chanceforcode@gmail.com>
2025-11-13 17:17:28 -0500

    Adding a addApp to the API

--- src/api/appsApi.js
+++ src/api/appsApi.js
@@ -5,6 +5,22 @@ import axios from "axios"; //  installed
 const BASE_URL = "https://server-app-prospecter.onrender.com/api/apps"; // easier to change later for Thursday
 const IMG_URL = "https://server-app-prospecter.onrender.com/images/";
 
+export async function addApp (app) {
+
+    const response = await fetch (BASE_URL, {
+        method: "POST",
+        body: FormData
+    }
+
+    );
+
+    if (!response.ok) {
+        throw new Error ("Failed to add app");
+    }
+
+    return response.json(); //  return new app from the server
+}
+
 export async function getApps() {
     const response =  await axios.get(BASE_URL);
    const apps = response.data.map (app => ({
